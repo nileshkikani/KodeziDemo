72c45beb9c0e11901cdcb5aa41cd1dfd3cb2894c
jeelLogistic <jeel@logisticinfotech.co.in>
2022-09-05 17:00:12 +0530

    integrate basic

--- views/LandingPage/index.js
+++ views/LandingPage/index.js
@@ -1,15 +1,29 @@
+import { Box, Container } from "@mui/material";
 import React from "react";
+import Footer from "../../Components/Layout/Footer";
 import Navigation from "../../Components/Layout/Navigation";
 
 const LandingPage = () => {
   return (
-    <div>
-      <div>
+    <Container>
+      <Box>
         <Navigation />
-      </div>
-      <div></div>
-      <div></div>
-    </div>
+        {/* todo */}
+        {/* first header and images */}
+        Card1
+      </Box>
+      <Box>
+        {/* todo */}
+        {/* second and cards with animation */}
+        Card2
+      </Box>
+      <Box>
+        {/* todo */}
+        {/* third cards */}
+        Card3
+        <Footer />
+      </Box>
+    </Container>
   );
 };
 
